42f0ea46bc9d3f698c50d812a08a8e80030be8c9
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-19 14:22:14 -0800

    feat: Materia UI package installation

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./Sidebar.css";
+import XIcon from '@mui/icons-material/X';
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className= "sidebar">
+        
+        <XIcon/>
+    </div>
   )
 }
 
