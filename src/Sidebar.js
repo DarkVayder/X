ea9a2060782b642a8d87a71e8bc5bc04fb567651
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-22 15:06:07 -0800

    Feat: Sidebar , Completed the addition of SidebarIcons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,7 +7,9 @@ import ExploreIcon from '@mui/icons-material/Explore';
 import SidebarOption from './SidebarOption';
 import EmailIcon from '@mui/icons-material/Email';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
-
+import ListIcon from '@mui/icons-material/List';
+import PersonIcon from '@mui/icons-material/Person';
+import MoreIcon from '@mui/icons-material/More';
 function Sidebar() {
   return (
     <div className= "sidebar">
@@ -19,7 +21,9 @@ function Sidebar() {
         <SidebarOption Icon={NotificationsIcon} text="Notification" />
         <SidebarOption Icon={EmailIcon} text="Email"/>
         <SidebarOption Icon={BookmarkIcon} text="Bookmark"/>
-
+        <SidebarOption Icon={ListIcon} text="Lists"/>
+        <SidebarOption Icon={PersonIcon} text="Profile"/>
+        <SidebarOption Icon={MoreIcon} text="More"/>
         
 
     </div>
