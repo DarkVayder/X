d96702878b1138b96d3c302dd2e516f0ace4ce2f
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-24 01:48:00 -0800

    feat: sidebar font size reduction

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -5,17 +5,15 @@ import { Avatar, Button } from '@mui/material';
 function TweetBox() {
   return (
     <div className='TweetBox'>
-        <form>
-            <div className='tweetBox__input'>
-            <Avatar src="src/Avatar.jpg"></Avatar>
-            <input placeholder='What is happening?' type='text'></input>
-
-
-            </div>
-            <Button>Post</Button>
-        </form>
+      <form>
+        <div className='tweetBox__input'>
+          <Avatar src="/Avatar.jpg" alt="User Avatar"></Avatar>
+          <input placeholder='What is happening?' type='text'></input>
+        </div>
+        <Button>Post</Button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default TweetBox
+export default TweetBox;
