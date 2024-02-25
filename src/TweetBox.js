49f62ff6ce4072c40a7465ef1a5f34a43994b850
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-25 13:23:55 -0800

    feat: Avatar import to tweetBox

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './TweetBox.css';
 import { Avatar, Button } from '@mui/material';
+import userAvatar from './Avatar.jpg';
 
 function TweetBox() {
   return (
     <div className='TweetBox'>
       <form>
         <div className='tweetBox__input'>
-          <Avatar src="/Avatar.jpg" alt="User Avatar"></Avatar>
+          <Avatar src={userAvatar} alt="User Avatar"></Avatar>
           <input placeholder='What is happening?!' type='text'></input>
         </div>
-        <Button className="tweetBox__tweetButton" >Post</Button>
+        <Button className="tweetBox__tweetButton">Post</Button>
       </form>
     </div>
   );
