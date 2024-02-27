98650b9b23cb81b744bd907f95edabf535830e9e
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-26 17:30:51 -0800

    feat: Tweetbox image input box styling

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -11,6 +11,10 @@ function TweetBox() {
           <Avatar src={userAvatar} alt="User Avatar"></Avatar>
           <input placeholder='What is happening?!' type='text'></input>
         </div>
+          <div className='tweetBox__ImageInput'>
+          
+          <input placeholder='Place your image here!' type='text'></input>
+        </div>
         <Button className="tweetBox__tweetButton">Post</Button>
       </form>
     </div>
