2600b391298f135a6fc1d6dbfb33882404423bc2
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-03-12 10:20:12 -0700

    feat: Post.js

--- src/Post.js
+++ src/Post.js
@@ -1,16 +1,23 @@
 import React from "react";
+import Avatar from "./Avatar.jpg";
 import "./Post.css";
 
 function Post({
-    displayName,
-    username,
-    verified,
-    text,
-    timestamp,
+  displayName,
+  username,
+  verified,
+  text,
+  image,
+  avatar,
 }) {
-    return <div className="post">
-
+  return (
+    <div className="post">
+      <div className="post__avatar">
+        <Avatar src={avatar} alt="User Avatar" />
+      </div>
+      {/* Other post content goes here */}
     </div>
+  );
 }
 
 export default Post;
