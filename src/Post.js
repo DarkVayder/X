9ad0da472717e48a21bfe22ad71e769237642bfc
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-03-15 13:52:26 -0700

    feat:fixed the scope error in post.js

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Avatar from "./Avatar.jpg";
 import "./Post.css";
-import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
 
 function Post({
   displayName,
@@ -28,7 +28,7 @@ function Post({
             <p>{username} Abzi</p>
           </div>
         </div>
-        {image && <img src={avatar} alt="Posted Image" />} 
+          {image && <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgithub.com%2Fcoil-kt%2Fcoil%2Fissues%2F540&psig=AOvVaw2hsQxdBoeABf5j1I67Z2Hl&ust=1710593173761000&source=images&cd=vfe&opi=89978449&ved=0CBMQjhxqFwoTCPj047yR94QDFQAAAAAdAAAAABAE" title="posted content"/>}
         <p>{text}</p>
       </div>
     </div>
