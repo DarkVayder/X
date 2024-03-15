24e1a5e82dca2ecbefd7c4ea51d4432ebb01844e
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-03-15 14:07:50 -0700

    feat: fixed the post.j img prop

--- src/Post.js
+++ src/Post.js
@@ -25,10 +25,10 @@ function Post({
             </h3>
           </div>
           <div className="post__headerDescription">
-            <p>{username} Abzi</p>
+            <p>{username} John Doe</p>
           </div>
         </div>
-          {image && <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgithub.com%2Fcoil-kt%2Fcoil%2Fissues%2F540&psig=AOvVaw2hsQxdBoeABf5j1I67Z2Hl&ust=1710593173761000&source=images&cd=vfe&opi=89978449&ved=0CBMQjhxqFwoTCPj047yR94QDFQAAAAAdAAAAABAE" title="posted content"/>}
+          {image && <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgithub.com%2Fcoil-kt%2Fcoil%2Fissues%2F540&psig=AOvVaw2hsQxdBoeABf5j1I67Z2Hl&ust=1710593173761000&source=images&cd=vfe&opi=89978449&ved=0CBMQjhxqFwoTCPj047yR94QDFQAAAAAdAAAAABAE" alt="posted content"/>}
         <p>{text}</p>
       </div>
     </div>
