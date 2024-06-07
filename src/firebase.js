29760231b47251efd7e23b1a05fc80543f909500
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-06-06 23:40:33 -0700

    feat: Installed Firebase and created the component

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgqAj3jJCUka9QBEwAhefnfwpM3fL9KDw",
+  authDomain: "x-twitter-clone-c9745.firebaseapp.com",
+  projectId: "x-twitter-clone-c9745",
+  storageBucket: "x-twitter-clone-c9745.appspot.com",
+  messagingSenderId: "247186064763",
+  appId: "1:247186064763:web:5d8dcf52f06894d63b29de"
+};
+
+
+const app = initializeApp(firebaseConfig);
