c7c58ce76791850747eaad5ae8e1c5674d8abfd7
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-06-06 23:10:01 -0700

    feat: Adjusted and styled my post and feed js for better User Experience

--- src/Feed.js
+++ src/Feed.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Feed.css';
 import TweetBox from './TweetBox';
 import Post from './Post';
-import userAvatar2 from './Avatar.jpg';
-import userImage2 from './image.jpg';
-import userImage from './image2.jpeg';
-import userAvatar from './avatar2.jpg';
+import userAvatar2 from './Avatar.jpg'; // DarkVayder's avatar
+import userImage2 from './image.jpg';    // DarkVayder's image
+import userImage from './image2.jpeg';   // Endeavor's image
+import userAvatar from './avatar2.jpg';  // Endeavor's avatar
 
 function Feed() {
+  console.log('userAvatar2:', userAvatar2);
+  console.log('userAvatar:', userAvatar);
+
   return (
     <div className='feed'>
       <div className='feed__header'>
@@ -17,21 +20,21 @@ function Feed() {
       <TweetBox />
 
       <Post
-      displayName="DarkVayder"
-      username="Jay"
-      verified={true}
-      text="COLD PALMER...!!!"
-      avatar={userAvatar2}
-      userImage={userImage2} 
+        displayName="DarkVayder"
+        username="Jay"
+        verified={true}
+        text="Acuna Matata...!!!"
+        avatar={userAvatar2}
+        userImage={userImage2}
       />
 
       <Post
-      displayName="Endeavor"
-      username="Jay"
-      verified={false}
-      text="Acuna Matata"
-      avatar={userAvatar}
-      userImage={userImage} 
+        displayName="Endeavor"
+        username="Jay"
+        verified={false}
+        text="Acuna Matata"
+        avatar={userAvatar}
+        userImage={userImage}
       />
     </div>
   );
